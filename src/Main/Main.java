1e773e94a93563741b8e0035e57e9fca412e2c69
JingPansh22 <163583238+JingPansh22@users.noreply.github.com>
2025-03-16 21:53:47 -0400

    Update Main.java

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,1 +1,13 @@
+package Main;
+
+import View.DifficultySelectionController;
+import javafx.application.Application;
+
+public class Main {
+    public static void main(String[] args) {
+        Application.launch(DifficultySelectionController.class, args);
+    }
+}
+
+
 
