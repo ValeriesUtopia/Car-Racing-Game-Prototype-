b3906ae833f78cb9162bcbf556ad48d643844a42
Andrew Lightfoot <falendsong@icloud.com>
2025-03-27 11:30:22 -0400

    Delete src directory
    Deleting this src diretory since it's an old version that is not needed.

--- src/model/CarFactory.java
+++
@@ -1,7 +0,0 @@
-package model;
-
-public class CarFactory {
-    public static Car createCar(int id, Stop startStop, String wheelType, String engineType) {
-        return new Car(id, startStop.getX(), startStop.getY(), wheelType, engineType);
-    }
-}
